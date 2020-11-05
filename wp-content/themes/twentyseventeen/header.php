eb91a1223a026021cef97fc46d1fb5c33f301b3d
hatazu <hatazu@gmail.com>
2020-11-05 10:27:03 +0700

    header

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -11,7 +11,8 @@
  * @since Twenty Seventeen 1.0
  * @version 1.0
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
